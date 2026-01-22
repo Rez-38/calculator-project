880ebdf97fca819f649ee099b0e157eb4ad77ab6
Rez38 <136545384+Rez-38@users.noreply.github.com>
2026-01-22 00:50:00 -0800

    Add numbers, operators, delete, & clear btns Add screen.
    All of these are not functional yet

--- javascript.js
+++ javascript.js
@@ -1,11 +1,7 @@
 console.log("Hello World!");
 
+
+
 // ================
 // Function Bank  =
 // ================
-
-function generateKeypad() {
-    for (let i = 0; i < 21; i++) {
-        
-    }
-}
